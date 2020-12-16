b56948c795342c5db28f70e3aa9f87edcfc9c7ed
Linda-Monique Nyangoh Timoh Hendricks <linda.timoh@gmail.com>
2020-12-16 19:17:33 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,10 @@
 def square_array(array)
-  # your code here
+ counter = 0 
+ new_array = Array.new 
+ 
+ while counter < array.lenght do 
+   new_array.push (Math sqrt (array))
+   counter += 1 
+ end 
+ return new_array
 end
